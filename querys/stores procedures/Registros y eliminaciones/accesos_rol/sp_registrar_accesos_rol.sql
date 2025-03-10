cb29a2c2417f3a20d88eda843d6dc2e283af8930
franklinnot <franklinnot@outlook.com>
2025-03-10 00:51:18 -0500

    accesos para roles

--- querys/stores procedures/Registros y eliminaciones/accesos_rol/sp_registrar_accesos_rol.sql
+++ querys/stores procedures/Registros y eliminaciones/accesos_rol/sp_registrar_accesos_rol.sql
@@ -61,10 +61,10 @@ DECLARE @ListaAccesos ud_accesos_rol;
 -- Insertar datos en la variable tipo tabla
 INSERT INTO @ListaAccesos (id_acceso) 
 VALUES 
-(17),
-(18),
-(19),
-(20);
+(21),
+(22),
+(23),
+(24);
 
 EXEC sp_registrar_accesos_rol @id_rol = 1, @id_empresa = 1, @accesos = @ListaAccesos;
 
