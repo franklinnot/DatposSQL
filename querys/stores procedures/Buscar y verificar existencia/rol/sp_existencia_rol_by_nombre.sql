03e4729041207b0a80ed255bf65cfd19c31361ee
franklinnot <franklinnot@outlook.com>
2025-03-10 13:30:00 -0500

    sp para verificar existencia de rol por nombre

---
+++ querys/stores procedures/Buscar y verificar existencia/rol/sp_existencia_rol_by_nombre.sql
@@ -0,0 +1,22 @@
+
+
+CREATE OR ALTER PROCEDURE sp_existencia_rol_by_nombre
+    @nombre NVARCHAR(128),
+    @id_empresa BIGINT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    IF EXISTS(SELECT 1 FROM rol WHERE LOWER(nombre) = LOWER(@nombre) AND id_empresa = @id_empresa)
+    BEGIN
+        SELECT 'true' AS 'verificar'
+    END
+    ELSE
+    BEGIN
+        SELECT 'false' AS 'verificar'
+    END
+END;
+
+EXEC sp_existencia_rol_by_nombre @nombre = 'Cajero', @id_empresa = 1;
+
+SELECT *
+FROM rol;
