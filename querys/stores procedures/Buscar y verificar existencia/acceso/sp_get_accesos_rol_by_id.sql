7c2096e9e9fecee5488692df27f4fb57c1c94cee
franklinnot <franklinnot@outlook.com>
2025-03-09 19:34:51 -0500

    columna estado al sp para obtener las rutas de acceso por rol

--- querys/stores procedures/Buscar y verificar existencia/acceso/sp_get_accesos_rol_by_id.sql
+++ querys/stores procedures/Buscar y verificar existencia/acceso/sp_get_accesos_rol_by_id.sql
@@ -6,7 +6,7 @@ AS
 BEGIN
     SET NOCOUNT ON;
     
-    SELECT a.id_acceso, a.nombre, a.ruta
+    SELECT a.id_acceso, a.nombre, a.ruta, a.estado
     FROM acceso a
     INNER JOIN acceso_rol ar ON a.id_acceso = ar.id_acceso
     WHERE ar.id_rol = @id_rol;
---
+++ utils/anyway.sql
@@ -0,0 +1,29 @@
+-- update empresa 
+-- set cantidad_sucursales = 8;
+
+-- update empresa 
+-- set sucursales_registradas = 2;
+-- update almacen
+-- set 
+-- departamento = 'La Libertad',
+-- ciudad = 'Trujillo'
+-- where id_almacen = 61;
+
+
+/*
+Almacenes
+warehouses/new
+
+nombre
+codigo
+departamento
+ciudad
+direccion
+id_empresa => jalar este dato de inertia
+
+*/
+
+select * from empresa;
+select * from sucursal;
+select * from caja;
+select * from almacen;
