b74fff14eb58d62a3ab81f9daf29b690c40203da
franklinnot <franklinnot@outlook.com>
2025-03-15 22:09:25 -0500

    sp para registrar unidades de medida

---
+++ querys/stores procedures/Registros, ediciones y eliminaciones/unidad_medida/sp_registrar_unidad_medida.sql
@@ -0,0 +1,56 @@
+
+
+CREATE OR ALTER PROCEDURE sp_registrar_unidad_medida
+    @nombre NVARCHAR(255),
+    @codigo NVARCHAR(24),
+    @descripcion NVARCHAR(255) = NULL,
+    @id_empresa BIGINT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @nuevo_id INT;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        -- Validar si la unidad de medidad ya existe por codigo
+        IF EXISTS (SELECT 1
+        FROM unidad_medida
+        WHERE codigo = @codigo)
+        BEGIN
+            ROLLBACK TRANSACTION;
+            -- Evitar duplicados
+            RETURN;
+        END;
+
+
+        -- Insertar 
+        INSERT INTO unidad_medida
+        (codigo, nombre, descripcion, estado, id_empresa)
+        VALUES
+        (@codigo, @nombre, @descripcion, '1', @id_empresa);
+        
+        SET @nuevo_id = SCOPE_IDENTITY();
+
+        COMMIT TRANSACTION;
+
+        -- Retornar el ID del nuevo usuario
+        SELECT @nuevo_id AS nuevo_id;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH;
+END;
+
+
+GO
+
+
+EXEC sp_registrar_unidad_medida
+@codigo = 'KG', @nombre = 'Kilogramito', @id_empresa = 1;
+
+
+SELECT * FROM unidad_medida;
+
