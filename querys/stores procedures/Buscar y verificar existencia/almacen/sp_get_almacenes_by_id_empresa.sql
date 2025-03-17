aacf78358273fb6973d39dc1e0e29baa943793c7
franklinnot <franklinnot@outlook.com>
2025-03-16 19:46:52 -0500

    sp get para listar almacenes por empresa

---
+++ querys/stores procedures/Buscar y verificar existencia/almacen/sp_get_almacenes_by_id_empresa.sql
@@ -0,0 +1,26 @@
+
+
+
+CREATE OR ALTER PROCEDURE sp_get_almacenes_by_id_empresa
+    @id_empresa BIGINT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        id_almacen,
+        codigo,
+        nombre,
+        departamento,
+        ciudad,
+        direccion,
+        estado,
+        id_empresa
+    FROM almacen
+    WHERE id_empresa = @id_empresa;
+END;
+
+EXEC sp_get_almacenes_by_id_empresa @id_empresa = 1;
+
+SELECT *
+FROM almacen;
