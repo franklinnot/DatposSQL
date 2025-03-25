82713f13f194a988adfcfb098efebeed0993253e
franklinnot <franklinnot@outlook.com>
2025-03-24 22:43:47 -0500

    backup hasta registrar productos

--- anyway.sql
+++ anyway.sql
@@ -1,45 +1,13 @@
 
--- columnas de una tabla
-SELECT '''' + COLUMN_NAME + '''' + ',' AS COLUMN_NAME
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'producto';
-
-/*
-Nuevo Producto
-> products
-
-codigo
-nombre
-Familia
-imagen ~
-
-tipo_producto 
-    => Si es un bien 
-        unidad_medida ~
-        stock_minimo ~
-        stock_maximo ~
-        fecha_vencimiento ~
-        alerta_stock ~
-        alerta_vencimiento ~
-    => Si es un servicio
-        precio
-id_empresa => inertia
-----------------------
-estado => 1
 
 
+delete from detalle_variante;
+delete from variante where id_producto = 11;
+delete from producto;
 
-costo_unitario => 0
-*/
-
-
--- columnas de una tabla
-SELECT '''' + COLUMN_NAME + '''' + ',' AS COLUMN_NAME
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'variante';
-
-SELECT '''' + COLUMN_NAME + '''' + ',' AS COLUMN_NAME
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'detalle_variante';
 
 select * from producto;
+select * from variante;
+select * from detalle_variante;
+
+select * from sucursal;
